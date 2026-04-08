242617045583368efd382aeb944879c3e5ab3310
sameer <sameer@gmail.com>
2026-04-08 08:37:50 +0530

    commited for react router dom package

--- ShoppersStackRetailer/src/routes/Router.jsx
+++ ShoppersStackRetailer/src/routes/Router.jsx
@@ -1,10 +1,16 @@
 import App from "../App";
 import React from "react";
+
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../components/login";
 let routes =createBrowserRouter([
   {
     path : "/",
     element : <App/>
   },
+  {
+    path:'/login',
+    element:<Login/>
+  }
 ])
 export default routes;
