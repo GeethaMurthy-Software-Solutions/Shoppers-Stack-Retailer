05b9e794dc31cc6ab23a96fd6be76893819f0e38
tualsidas <panchireddi01@gmail.com>
2026-04-08 08:50:23 +0530

    added router.jsx

--- ShoppersStackRetailer/src/routes/Router.jsx
+++ ShoppersStackRetailer/src/routes/Router.jsx
@@ -1,10 +1,23 @@
 import App from "../App";
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import AdminLayout from "../components/AdminLayout";
+import Dashboard from "../modules/Dashboard";
 let routes =createBrowserRouter([
   {
     path : "/",
     element : <App/>
   },
+   {
+        path: "/landingpage",
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <Dashboard />
+            },
+          ]
+    }
+      
 ])
 export default routes;
